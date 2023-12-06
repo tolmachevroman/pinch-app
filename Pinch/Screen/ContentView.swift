6904c0353c90a371b4a20d0ad43055022450b30d
Roman Tolmachev <tolmachevroman@gmail.com>
2023-12-06 17:43:05 -0300

    add double tap gesture

--- Pinch/Screen/ContentView.swift
+++ Pinch/Screen/ContentView.swift
@@ -8,14 +8,51 @@
 import SwiftUI
 
 struct ContentView: View {
+    // MARK: - PROPERTY
+    
+    @State private var isAnimating: Bool = false
+    @State private var imageScale: CGFloat = 1
+    
+    // MARK: - FUNCTION
+    
+    // MARK: - CONTENT
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationView {
+            ZStack {
+                // MARK: - PAGE IMAGE
+                Image("magazine-front-cover")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .cornerRadius(10)
+                    .padding()
+                    .shadow(color: .black.opacity(0.2), radius: 12, x: 2, y: 2)
+                    .opacity(isAnimating ? 1 : 0)
+                    .animation(.linear(duration: 1), value: isAnimating)
+                    .scaleEffect(imageScale)
+                    // MARK: - TAP GESTURE
+                    .onTapGesture(count: 2) {
+                        if (imageScale == 1) {
+                            withAnimation(.spring) {
+                                imageScale = 5
+                            }
+                        } else {
+                            withAnimation(.spring) {
+                                imageScale = 1
+                            }
+                        }
+                    }
+                
+            }
+            .navigationTitle("Pinch & Zoom")
+            .navigationBarTitleDisplayMode(.inline)
+            .onAppear(perform: {
+                withAnimation(.linear(duration: 1)) {
+                    isAnimating = true
+                }
+            })
         }
-        .padding()
+        .navigationViewStyle(.stack)
     }
 }
 
