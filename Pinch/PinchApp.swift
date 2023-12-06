af0d991e995fcd6ddd2689671f28e94bdca234fa
Roman Tolmachev <tolmachevroman@gmail.com>
2023-12-06 17:04:18 -0300

    initial project setup

---
+++ Pinch/PinchApp.swift
@@ -0,0 +1,17 @@
+//
+//  PinchApp.swift
+//  Pinch
+//
+//  Created by Roman Tolmachev on 6.12.2023.
+//
+
+import SwiftUI
+
+@main
+struct PinchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
