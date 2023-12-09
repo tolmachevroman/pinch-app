06415c66d5c212dbb53b3d6fa98e4b53614ece14
Roman Tolmachev <tolmachevroman@gmail.com>
2023-12-09 07:50:06 -0300

    add long press gesture

--- Pinch/Screen/ContentView.swift
+++ Pinch/Screen/ContentView.swift
@@ -27,6 +27,8 @@ struct ContentView: View {
     var body: some View {
         NavigationView {
             ZStack {
+                Color.clear
+                
                 // MARK: - PAGE IMAGE
                 Image("magazine-front-cover")
                     .resizable()
@@ -72,6 +74,13 @@ struct ContentView: View {
                     isAnimating = true
                 }
             })
+            // MARK: - INFO PANEL
+            .overlay(
+                InfoPanelView(scale: imageScale, offset: imageOffset)
+                    .padding(.horizontal)
+                    .padding(.top, 30), 
+                alignment: .top
+            )
         }
         .navigationViewStyle(.stack)
     }
